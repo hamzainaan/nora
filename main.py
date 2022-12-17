fc938e2cd0f2c934c9a8af57e959dc5677609567
Hamza Inan <51299406+hamzainaan@users.noreply.github.com>
2022-12-17 15:08:00 +0300

    License

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-#Gerekli Kütühaneler
+"""
+    Nora, A Basic Discord Bot to support coinflip, slot and some moderation stuff.
+    Copyright (C) 2022  Hamza Inan <h@inanweb.ml>
+
+    Nora is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Nora is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+#Gerekli Kütüphaneler
 import discord
 from discord.ext import commands
 from cryptography.fernet import Fernet
